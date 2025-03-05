9bc6d0d9f4bb365b68b058b7c409e2c283586db6
Felipe Jimenez <jfjimenezsalazar@gmail.com>
2025-03-04 23:55:08 -0500

    Se formatean los componentes

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -1,28 +1,28 @@
 <template>
-    <v-layout>
-        <h1>mostrar{{this.mostrarlista}}</h1>
-        <BottomNavigation @switch="switchList"/>
-    </v-layout>
+  <v-layout>
+    <h1>mostrar{{ this.mostrarlista }}</h1>
+    <BottomNavigation @switch="switchList" />
+  </v-layout>
 </template>
 
 <script>
-import BottomNavigation from '../components/BottomNavigation.vue';
+import BottomNavigation from "../components/BottomNavigation.vue";
 
 export default {
-    name: 'ListComponent',
-    data() {
-        return {
-            mostrarlista: 'all', // Estado inicial
-        };
+  name: "ListComponent",
+  data() {
+    return {
+      mostrarlista: "all", // Estado inicial
+    };
+  },
+  components: {
+    BottomNavigation,
+  },
+  methods: {
+    switchList(id_seleccion) {
+      this.mostrarlista = id_seleccion;
     },
-    components: {
-        BottomNavigation,
-    },
-    methods: {
-        switchList(id_seleccion) {
-            this.mostrarlista = id_seleccion; 
-        },
-    },
+  },
 };
 </script>
 
